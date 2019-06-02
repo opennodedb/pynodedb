63d9820e21f89377b4ae0b15debc686dcbd0c3a5
Sam Burney <sburney@sifnt.net.au>
2019-06-02 17:19:31 +0930

    Added basic column sorting to nodes.list()

--- pynodedb/nodes.py
+++ pynodedb/nodes.py
@@ -5,12 +5,13 @@ from flask import (
     url_for,
     abort,
     request,
+    jsonify,
 )
 from flask_login import login_required, current_user
 
 # import local modules
 from . import functions as f
-from .database import Node
+from .database import db, Node, Suburb, User, Status
 
 # Define BLueprint
 bp = Blueprint('nodes', __name__, url_prefix='/nodes')
@@ -19,17 +20,21 @@ bp = Blueprint('nodes', __name__, url_prefix='/nodes')
 @bp.route('/', methods=['GET'])
 @login_required
 def list():
+    sort = request.args.get('sort') or 'nodes.name'
+
     if not current_user.is_active:
         return redirect(url_for('home'))
 
     nodes = Node.query.filter_by(
-        user_id=current_user.id).order_by('name').all()
+        user_id=current_user.id).join(Suburb, User, Status).order_by(db.text(sort)).all()
     return render_template('nodes/list.html', nodes=nodes)
 
 
 @bp.route('/all', methods=['GET'])
 @login_required
 def all():
+    sort = request.args.get('sort') or 'nodes.name'
+
     if current_user.in_group(3) and current_user.is_active:
         page = request.args.get('page')
         perpage = request.args.get('perpage') or 50
@@ -37,11 +42,12 @@ def all():
         nodes = []
 
         if page:
-            pagination = Node.query.order_by('name').paginate(
+            pagination = Node.query.join(Suburb, User, Status).order_by(db.text(sort)).paginate(
                 int(page), int(perpage), False)
             nodes = pagination.items
         else:
-            nodes = Node.query.order_by('name').all()
+            nodes = Node.query.join(
+                Suburb, User, Status).order_by(db.text(sort)).all()
 
         return render_template('nodes/list.html', nodes=nodes, pagination=pagination)
 
